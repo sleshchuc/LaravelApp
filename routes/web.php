f9614fbb8ae1680622b28ef345954749ad61d6f5
sleshuck <alumne@insebre-com>
2024-01-15 18:39:24 +0100

    Sync

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+Route::get('/',[UserController::class,'index']);
+Route::get('/users',[UserController::class,'index']);
+
+Route::get('/contact',[PagesController::class,'contact']);
+Route::get('/about',[PagesController::class,'about']);
+
+Route::get('/tasks',[\App\Http\Controllers\TaskController::class,'index']);
